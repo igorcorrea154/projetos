fdf49ada9cf1b306c12d0f1eb64b4e86cb0cdcf6
igor <igor.correa1514@gmail.com>
2024-10-24 12:53:53 -0300

    Delete 53.py

--- 53.py
+++
@@ -1,6 +0,0 @@
-frase = str(input("digite uma frase: ")).strip().lower().split()
-junto = "".join(frase)
-if junto == junto[::-1]:
-    print("é um palimdromo")
-else:
-    print("nao é um palimdromo")
