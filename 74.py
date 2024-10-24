5a83ef583733be6bd0f185eb8b74c975f6623bc0
igor <igor.correa1514@gmail.com>
2024-10-24 12:58:53 -0300

    Delete 74.py

--- 74.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-n = (randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10))
-print(sorted(n))
-print(f"o maior numero é o {max(n)} e o menor é o {min(n)}")
