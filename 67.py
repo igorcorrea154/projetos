e0d5a615e294095bd5657439f00534bd16cabd9a
igor <igor.correa1514@gmail.com>
2024-10-24 12:57:53 -0300

    Delete 67.py

--- 67.py
+++
@@ -1,9 +0,0 @@
-print("TABUADA")
-print("")
-while True:
-    n = int(input("digite um valor para ver sua tabuada: "))
-    if n < 0:
-        break
-    for c in range(1,11):
-        print(f"{n} x {c} = {n*c}")
-print("PROGRAMA FINALIZADO, NAO FOI POSSIVEL FAZER A OPERACAO")
