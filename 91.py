8bab6b5fe4e575bb3c5fdc56b92b19ad68200baf
igor <igor.correa1514@gmail.com>
2024-10-24 13:00:53 -0300

    Delete 91.py

--- 91.py
+++
@@ -1,8 +0,0 @@
-def area():
-    s = altura * largura
-    print(f"a area de um terreno {altura}X{largura} = {s} m²")
-
-print("CALCULAR AREA")
-altura = float(input("ALTURA (m): "))
-largura = float(input("LARGURA (m):  "))
-area()
