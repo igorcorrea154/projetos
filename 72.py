818f0792c80887a4c74d0311b614d1d8013392d1
igor <igor.correa1514@gmail.com>
2024-10-24 12:58:39 -0300

    Delete 72.py

--- 72.py
+++
@@ -1,3 +0,0 @@
-extenso = ("zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove","vinte")
-n = int(input("DIGITE UM NUMERO DE 0 A 20: "))
-print(f"o numero que voce digitou foi {extenso[n].upper()}")
