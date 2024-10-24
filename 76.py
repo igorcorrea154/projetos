304c2a4f3e4060c5b2a72e4649b2ddc48fffff91
igor <igor.correa1514@gmail.com>
2024-10-24 12:59:06 -0300

    Delete 76.py

--- 76.py
+++
@@ -1,5 +0,0 @@
-print("-"*10,"PRODUTOS","-"*10)
-n = (("lapis","caneta","mochila","caderno","apontador","borracha"),(22.50, 3,50, 120.35, 55.00, 3.50, 4.50))
-for c in n:
-    print(c,"."*20, "R$",c)
-    print("-"*30)
